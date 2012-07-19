7cc6c296fae24c7278d8353603c433b045766862
Keith Gaughan <k@stereochro.me>
2012-07-19 23:18:08 +0100

    Ensure insertion between two nodes works.

--- tests/test_node.py
+++ tests/test_node.py
@@ -10,6 +10,7 @@ def test_creation():
 
 
 def test_insert():
+    # Insert at tail.
     first = bukkit.Node(None)
     second = bukkit.Node(None)
     second.insert_before(first)
@@ -17,3 +18,11 @@ def test_insert():
     assert first.prev_node is second
     assert second.next_node is first
     assert second.prev_node is None
+
+    # Insert between two nodes.
+    third = bukkit.Node(None)
+    third.insert_before(first)
+    assert first.prev_node is third
+    assert second.next_node is third
+    assert third.prev_node is second
+    assert third.next_node is first
