bf55f15448a9d0083c8b6784f71289d3909dddf7
Keith Gaughan <k@stereochro.me>
2012-07-20 00:30:21 +0100

    Test consumption.

--- tests/test_bucket.py
+++ tests/test_bucket.py
@@ -12,3 +12,24 @@ def test_creation():
     assert bucket.rate == 23
     assert bucket.limit == 9000
     assert bucket._available == bucket.limit
+
+
+def test_consumption():
+    ticks = 0
+    fake_clock = lambda: ticks
+
+    bucket = bukkit.TokenBucket(5, 20, clock=fake_clock)
+    assert bucket.tokens == 20
+    assert bucket.consume(10)
+    assert not bucket.consume(15)
+    assert bucket.tokens == 10
+    
+    ticks += 1
+    assert bucket.tokens == 15
+    ticks += 2
+    assert bucket.tokens == 20
+
+    assert bucket.consume(1)
+    assert bucket.tokens == 19
+    ticks += 1
+    assert bucket.tokens == 20
