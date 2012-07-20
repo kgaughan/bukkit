dd767497271d7323b073bdec7cdd46c753a271e1
Keith Gaughan <k@stereochro.me>
2012-07-20 01:59:37 +0100

    Test purging.

--- tests/test_collection.py
+++ tests/test_collection.py
@@ -40,3 +40,44 @@ def test_get():
 
     buckets.consume('thingy', 5)
     assert buckets['thingy'].tokens == 18
+
+
+def test_purge():
+    ticks = 0
+    fake_clock = lambda: ticks
+    buckets = Collection(rate=1, limit=20, timeout=15, clock=fake_clock)
+
+    # Create a bunch of buckets for messing with.
+    for i in xrange(4):
+        buckets.consume('x' + str(i), 0)
+    assert sorted(buckets.node_map.keys()) == ['x0', 'x1', 'x2', 'x3']
+
+    ticks += 5
+    buckets.consume('x1', 0)
+    assert buckets['x1'].ts == ticks
+    buckets.purge()
+    assert len(buckets.node_map) == 4
+
+    ticks += 5
+    buckets.consume('x2', 0)
+    assert buckets['x2'].ts == ticks
+    buckets.purge()
+    assert len(buckets.node_map) == 4
+
+    ticks += 5
+    buckets.consume('x3', 0)
+    assert buckets['x3'].ts == ticks
+    buckets.purge()
+    assert len(buckets.node_map) == 3
+
+    ticks += 5
+    buckets.purge()
+    assert len(buckets.node_map) == 2
+
+    ticks += 5
+    buckets.purge()
+    assert len(buckets.node_map) == 1
+
+    ticks += 5
+    buckets.purge()
+    assert len(buckets.node_map) == 0
