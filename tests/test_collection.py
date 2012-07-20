9ee1db76af2a1afdf59bf9099008715d9bca2f4d
Keith Gaughan <k@stereochro.me>
2012-07-20 01:33:29 +0100

    Make sure getting buckets and checking for their presence work.

--- tests/test_collection.py
+++ tests/test_collection.py
@@ -8,3 +8,26 @@ def test_creation():
     assert buckets.timeout == 31
     assert buckets.head_node.prev_node is buckets.tail_node
     assert buckets.tail_node.next_node is buckets.head_node
+
+
+def test_contains():
+    buckets = Collection(rate=5, limit=23, timeout=31, clock=lambda: 0)
+
+    assert len(buckets.node_map) == 0
+    assert 'thingy' not in buckets
+    buckets.consume('thingy', 5)
+    assert len(buckets.node_map) == 1
+    assert 'thingy' in buckets
+
+
+def test_get():
+    buckets = Collection(rate=5, limit=23, timeout=31, clock=lambda: 0)
+
+    try:
+        buckets['thingy']
+        assert False, "Should not be able to look up 'thingy'"
+    except IndexError, exc:
+        assert str(exc) == "No such bucket: 'thingy'"
+
+    buckets.consume('thingy', 5)
+    assert buckets['thingy'].tokens == 18
