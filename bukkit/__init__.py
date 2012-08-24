0d515ebb7cfacd6d47ec1bdbe508181e016d5f8e
Keith Gaughan <k@stereochro.me>
2012-08-24 15:10:57 +0100

    Client/server protocol implementation written while at OggCamp.

--- bukkit/__init__.py
+++ bukkit/__init__.py
@@ -1,5 +1,5 @@
 from bukkit.bucket import TokenBucket, Collection
-from bukkit.net import client, BucketRequestHandler, run_server
+from bukkit.net import Client, RequestHandler, run_server
 import SocketServer
 
 
@@ -7,7 +7,7 @@ __author__ = 'Keith Gaughan'
 __email__ = 'k@stereochro.me'
 __version__ = '0.1.0'
 
-__all__ = ('TokenBucket', 'Collection', 'client', 'BucketRequestHandler')
+__all__ = ('TokenBucket', 'Collection', 'Client', 'RequestHandler')
 
 
 if __name__ == '__main__':
--- bukkit/net.py
+++ bukkit/net.py
@@ -4,7 +4,59 @@ import socket
 from bukkit import bucket
 
 
-class BucketRequestHandler(SocketServer.BaseRequestHandler):
+_REQS = {
+    'B': ('r', 'l', 't', 'c'),
+    'C': ('t', 'c', 'b')}
+
+
+_RESPS = {
+    '+': (),
+    '-': (),
+    '!': ('m',)}
+
+
+class ProtocolError(Exception):
+    """
+    """
+
+    __slots__ = ()
+
+
+def _read_stanza(rfile, allowed):
+    stanza_type = rfile.readline().rstrip("\n")
+    if stanza_type not in allowed:
+        raise ProtocolError("'%s' is not recognised" % stanza_type)
+
+    attrs = allowed[stanza_type]
+    collected = {}
+    for line in rfile:
+        line = line.rstrip("\n")
+        if len(line) == 0:
+            break
+        if len(collected) > len(attrs):
+            raise ProtocolError("Too many attributes; max is %d" % len(attrs))
+        parts = line.split(1, '=')
+        if len(parts) != 2:
+            raise ProtocolError("'%s' is not an attribute line" % line)
+        key, value = parts
+        if key in collected:
+            raise ProtocolError("'%s' provided multiple times" % key)
+        if key not in attrs:
+            raise ProtocolError("'%s' is not a valid attribute" % key)
+        collected[key] = value
+
+    return stanza_type, attrs
+
+
+def _build_stanza(stanza_type, attrs=None):
+    if attrs is None:
+        attrs = {}
+    return "\n".join(
+        [stanza_type] +
+        [key + '=' + value for key, value in attrs.iteritems()]) + "\n\n"
+
+
+class RequestHandler(SocketServer.StreamRequestHandler):
     """
     Simple request handler for a simple stream-based protocol for managing
     a set of token bucket collections. Responses are terminated by
@@ -21,83 +73,74 @@ class BucketRequestHandler(SocketServer.BaseRequestHandler):
         - Attempt to consume the given tokens from the named bucket in the
           named collection. Response is '+'if successful, '-' if not.
 
-      If the response code starts with '!', an error message follows on the
-      next line.
+      If the response code starts with '!', an error message follows in the
+      'm' attribute.
     """
-
-    attrs = {
-        'B': ('r', 'l', 't', 'c'),
-        'C': ('t', 'c', 'b')}
 
     collections = {}
 
     def error(self, msg):
-        self.wfile.write("!\n%s\n\n" % msg)
+        self.wfile.write(_build_stanza('!', {'m': msg}))
 
     def success(self):
-        self.wfile.write("+\n\n")
+        self.wfile.write(_build_stanza('+'))
 
     def failure(self):
-        self.wfile.write("-\n\n")
+        self.wfile.write(_build_stanza('-'))
 
     def handle(self):
-        cmd = self.rfile.readline().rstrip("\n")
-        if cmd not in self.attrs:
-            self.error("'%s' is not a valid command" % cmd)
-            return
-        attrs = self.attrs[cmd]
-        collected = {}
-        for line in lines:
-            line = line.rstrip("\n")
-            if len(line) == 0:
-                break
-            if len(collected) > len(attrs):
-                self.error("Too many attributes; max is %d" % len(attrs))
-                return
-            parts = line.split(1, '=')
-            if len(parts) != 2:
-                self.error("'%s' is not an attribute line" % line)
-                return
-            key, value = parts
-            if key in collected:
-                self.error("'%s' provided multiple times" % key)
-                return
-            if key not in attrs:
-                self.error("'%s' is not a valid attribute" % key)
-                return
-            collected[key] = value
-        if cmd == 'B':
-            self.handle_create(
-                rate=attrs['r'],
-                limit=attrs['l'],
-                timeout=attrs['t'],
-                collection=attrs['c'])
-        elif cmd == 'C':
-            self.handle_consume(
-                collection=attrs['c'],
-                bucket=attrs['b'],
-                tokens=attrs['t'])
+        try:
+            cmd, attrs = _read_stanza(self.rfile, _REQS)
+            if cmd == 'B':
+                self.handle_create(
+                    rate=attrs['r'],
+                    limit=attrs['l'],
+                    timeout=attrs['t'],
+                    collection=attrs['c'])
+            elif cmd == 'C':
+                self.handle_consume(
+                    collection=attrs['c'],
+                    name=attrs['b'],
+                    tokens=attrs['t'])
+            else:
+                self.wfile.write(_build_stanza('!', {'m': 'WAT'}))
+        except ProtocolError, exc:
+            self.wfile.write(_build_stanza('!', {'m': exc.message}))
 
     def handle_create(self, rate, limit, timeout, collection):
         self.collections[collection] = bucket.Collection(
             rate=rate, limit=limit, timeout=timeout)
         self.success()
 
-    def handle_consume(self, collection, bucket, tokens):
-        if collection not in collections:
+    def handle_consume(self, collection, name, tokens):
+        if collection not in self.collections:
             self.error("'%s' is not a known bucket collection" % collection)
             return
-        if self.collections[collection].consume(bucket, tokens):
+        if self.collections[collection].consume(name, tokens):
             self.success()
         else:
             self.failure()
 
 
-def client():
-    pass
+class Client(object):
+
+    __all__ = ('sock',)
+
+    def __init__(self, path):
+        self.sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        self.sock.connect(path)
+
+    def _send(self, cmd, attrs):
+        self.sock.sendall(_build_stanza(cmd, attrs))
+
+    def create(self, rate, limit, timeout, collection):
+        self._send('B', {'r': rate, 'l': limit, 't': timeout, 'c': collection})
+
+    def consume(self, collection, name, tokens):
+        self._send('C', {'c': collection, 'b': name, 't': tokens})
 
 
 def run_server():
     server = SocketServer.UnixStreamServer(
-        '/tmp/bukkit.sock', BucketRequestHandler)
+        '/tmp/bukkit.sock', RequestHandler)
     server.serve_forever()
