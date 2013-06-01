c98a9250a2126ee85af0ebc153d11251cd37c197
Keith Gaughan <k@stereochro.me>
2013-06-01 11:43:23 +0100

    setup.py cleanup

--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 
-from __future__ import with_statement
-
 from setuptools import setup
 from buildkit import *
 
@@ -13,20 +11,20 @@ setup(
     name='bukkit',
     version=META['version'],
     description='Keyed collections of token buckets.',
-    long_description=read('README'),
+    long_description=read('README') + "\n\n" + read('ChangeLog'),
     url='https://github.com/kgaughan/bukkit/',
     license='MIT',
     packages=['bukkit'],
 
-    classifiers=[
+    classifiers=(
         'Development Status :: 3 - Alpha',
         'Environment :: Other Environment',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-    ],
+    ),
 
     author=META['author'],
-    author_email=META['email']
+    author_email=META['email'],
 )
