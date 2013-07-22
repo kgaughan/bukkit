a5ed1c9dd92b6e3e148acdf0567cdfec2dbc6020
Keith Gaughan <k@stereochro.me>
2013-07-23 00:10:34 +0100

    Start replacing the old SocketServer server with on based on asyncore.

--- bukkit/server.py
+++ bukkit/server.py
@@ -1,91 +1,90 @@
-import SocketServer
+"""
+Simple network interface for managing a set of token bucket collections.
+
+Protocol
+~~~~~~~~
+
+Responses are terminated by double-linefeeds. In the descriptions, semicolons
+are used to represent linefeeds for compactness.  Field order within requests
+can vary, but the command line must always come first. Responses follow the
+same format as requests.
+
+Request format:
+
+  B;r=<rate>;l=<limit>;t=<timeout>;c=<collection>
+    - Create a bucket collection with the given settings. Always responds
+      with '+'. Note that if a collection already exists with the given name,
+      it will be destroyed.
+  C;c=<collection>;b=<bucket>;t=<tokens>
+    - Attempt to consume the given tokens from the named bucket in the named
+      collection. Response is '+'if successful, '-' if not.
+
+  If the response code starts with '!', an error message follows in the 'm'
+  attribute.
+"""
+
+import asynchat
+import asyncore
 import socket
 
-from bukkit import bucket, client
+from bukkit import bucket
 
 
 _REQS = {
-    'B': ('r', 'l', 't', 'c'),
-    'C': ('t', 'c', 'b'),
+    # Create bucket:
+    # B;r=<rate>;l=<limit>;t=<timeout>;c=<collection>
+    'B': ['r', 'l', 't', 'c'],
+    # Consume tokens from a bucket.
+    # C;c=<collection>;b=<bucket>;t=<tokens>
+    'C': ['t', 'c', 'b'],
 }
 
 
 _RESPS = {
-    '+': (),
-    '-': (),
-    '!': ('m',),
+    # Success.
+    '+': [],
+    # Action failed without an error, e.g. bucket exhausted.
+    '-': [],
+    # Error; 'm' attribute contains error message.
+    # !;m=<msg>
+    '!': ['m'],
 }
 
 
-class RequestHandler(SocketServer.StreamRequestHandler):
+class Handler(asynchat.async_chat):  # pylint: disable-msg=R0904
     """
-    Simple request handler for a simple stream-based protocol for managing
-    a set of token bucket collections. Responses are terminated by
-    double-linefeeds. In the descriptions, semicolons are used to represent
-    linefeeds for compactness. Field order within requests can vary, but the
-    command line must always come first. Responses
-
-    Request format:
-      B;r=<rate>;l=<limit>;t=<timeout>;c=<collection>
-        - Create a bucket collection with the given settings. Always responds
-          with '+'. Note that if a collection already exists with the given
-          name, it will be destroyed.
-      C;c=<collection>;b=<bucket>;t=<tokens>
-        - Attempt to consume the given tokens from the named bucket in the
-          named collection. Response is '+'if successful, '-' if not.
-
-      If the response code starts with '!', an error message follows in the
-      'm' attribute.
+    Handle charity domain queries.
     """
 
-    collections = {}
+    def __init__(self, sock, db_path):
+        asynchat.async_chat.__init__(self, sock)
+        self.ibuffer = []
+        self.set_terminator(CRLF)
 
-    def _send(self, stanza_type, attrs=None):
-        self.wfile.write(client._build_stanza(stanza_type, attrs))
+    def collect_incoming_data(self, data):
+        self.ibuffer.append(data)
 
-    def error(self, msg):
-        self._send('!', {'m': msg})
-
-    def success(self):
-        self._send('+')
-
-    def failure(self):
-        self._send('-')
-
-    def handle(self):
-        try:
-            cmd, attrs = client._read_stanza(self.rfile, _REQS)
-            if cmd == 'B':
-                self.handle_create(
-                    rate=attrs['r'],
-                    limit=attrs['l'],
-                    timeout=attrs['t'],
-                    collection=attrs['c'])
-            elif cmd == 'C':
-                self.handle_consume(
-                    collection=attrs['c'],
-                    name=attrs['b'],
-                    tokens=attrs['t'])
-            else:
-                self.error('WAT')
-        except client.ProtocolError as exc:
-            self.error(exc.message)
-
-    def handle_create(self, rate, limit, timeout, collection):
-        self.collections[collection] = bucket.Collection(
-            rate=rate, limit=limit, timeout=timeout)
-        self.success()
-
-    def handle_consume(self, collection, name, tokens):
-        if collection not in self.collections:
-            self.error("'%s' is not a known bucket collection" % collection)
-        elif self.collections[collection].consume(name, tokens):
-            self.success()
-        else:
-            self.failure()
+    def found_terminator(self):
+        request = ''.join(self.ibuffer)
+        self.ibuffer = []
+        self.push('Request was: [{0}]'.format(request))
+        self.push(CRLF)
 
 
-def run_server():
-    server = SocketServer.UnixStreamServer(
-        '/tmp/bukkit.sock', RequestHandler)
-    server.serve_forever()
+class UnixServer(asyncore.dispatcher):  # pylint: disable-msg=R0904
+    """
+    Simple listener that accepts incoming connections over a Unix stream
+    socket and spawns per-connection handlers.
+    """
+
+    def __init__(self, path):
+        asyncore.dispatcher.__init__(self)
+        self.create_socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        self.bind(path)
+        self.listen(128)
+
+    def handle_accept(self):
+        pair = self.accept()
+        if pair is not None:
+            sock, _ = pair
+            Handler(sock)
